c9939f15b03fd265c18860733e807a54a962b1eb
fuziix <fuziix@gmail.com>
2025-12-09 18:07:42 -0300

    Add db_departamentos configuration for Docker deployment

--- config/autoload/global.php
+++ config/autoload/global.php
@@ -12,6 +12,17 @@ return [
         ],
     ],
 
+    // Configuración para el módulo Departamentos
+    'db_departamentos' => [
+        'driver'   => 'Pdo',
+        'dsn'      => 'mysql:dbname=' . (getenv('DB_NAME') ?: 'conaricl_dideco') . ';host=' . (getenv('DB_HOST') ?: 'localhost') . ';charset=utf8',
+        'username' => getenv('DB_USER') ?: 'conaricl_dideco',
+        'password' => getenv('DB_PASS') ?: 'Ladronaso@266',
+        'driver_options' => [
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+        ],
+    ],
+
     'service_manager' => [
         'factories' => [
             Laminas\Db\Adapter\Adapter::class => Laminas\Db\Adapter\AdapterServiceFactory::class,
