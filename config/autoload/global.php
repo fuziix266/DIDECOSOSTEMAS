921dbfddf54c1c1d1d14d42df4a5496ef2edfc65
fuziix <fuziix@gmail.com>
2025-12-11 11:39:16 -0300

    Hardcode production credentials in global.php for cPanel fix

--- config/autoload/global.php
+++ config/autoload/global.php
@@ -4,9 +4,9 @@ return [
     // Esta configuración es para otros módulos que no sean Departamentos
     'db' => [
         'driver'   => 'Pdo',
-        'dsn'      => 'mysql:dbname=' . (getenv('DB_NAME') ?: 'conaricl_dideco') . ';host=' . (getenv('DB_HOST') ?: 'localhost') . ';charset=utf8',
-        'username' => getenv('DB_USER') ?: 'conaricl_dideco',
-        'password' => getenv('DB_PASS') ?: 'Ladronaso@266',
+        'dsn'      => 'mysql:dbname=conaricl_dideco;host=localhost;charset=utf8',
+        'username' => 'conaricl_dideco',
+        'password' => 'didecoadmin123',
         'driver_options' => [
             PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
         ],
@@ -15,9 +15,9 @@ return [
     // Configuración para el módulo Departamentos
     'db_departamentos' => [
         'driver'   => 'Pdo',
-        'dsn'      => 'mysql:dbname=' . (getenv('DB_NAME') ?: 'conaricl_dideco') . ';host=' . (getenv('DB_HOST') ?: 'localhost') . ';charset=utf8',
-        'username' => getenv('DB_USER') ?: 'conaricl_dideco',
-        'password' => getenv('DB_PASS') ?: 'Ladronaso@266',
+        'dsn'      => 'mysql:dbname=conaricl_dideco;host=localhost;charset=utf8',
+        'username' => 'conaricl_dideco',
+        'password' => 'didecoadmin123',
         'driver_options' => [
             PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
         ],
@@ -26,9 +26,9 @@ return [
     // Configuración para el módulo Radio
     'db_radio' => [
         'driver'   => 'Pdo',
-        'dsn'      => 'mysql:dbname=' . (getenv('DB_NAME') ?: 'radio_app') . ';host=' . (getenv('DB_HOST') ?: 'localhost') . ';charset=utf8',
-        'username' => getenv('DB_USER') ?: 'root',
-        'password' => getenv('DB_PASS') ?: '',
+        'dsn'      => 'mysql:dbname=conaricl_dideco;host=localhost;charset=utf8',
+        'username' => 'conaricl_dideco',
+        'password' => 'didecoadmin123',
         'driver_options' => [
             PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
         ],
@@ -37,9 +37,9 @@ return [
     // Configuración para el módulo Vehiculos
     'db_vehiculos' => [
         'driver'   => 'Pdo',
-        'dsn'      => 'mysql:dbname=' . (getenv('DB_NAME') ?: 'qr_vehiculos_municipal') . ';host=' . (getenv('DB_HOST') ?: 'localhost') . ';charset=utf8',
-        'username' => getenv('DB_USER') ?: 'root',
-        'password' => getenv('DB_PASS') ?: '',
+        'dsn'      => 'mysql:dbname=conaricl_dideco;host=localhost;charset=utf8',
+        'username' => 'conaricl_dideco',
+        'password' => 'didecoadmin123',
         'driver_options' => [
             PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
         ],
